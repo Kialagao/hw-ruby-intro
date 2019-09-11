35b6d0a717dbcb6d38aa4713c5b4fc42660f16a1
King Arthur Alagao <kingarthuralagao@gmail.com>
2019-09-11 07:54:01 -0700

    FInished some

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,30 +3,52 @@
 # Part 1
 
 def sum (arr)
-  # YOUR CODE HERE
-  if arr.empty?
+  arr.empty? ? 0 : arr.sum
+end
+
+def max_2_sum (arr)
+  case arr.length
+
+  when 0
   	return 0
+  when 1
+  	return arr.first
   else
-  	arr.sum
+  	max = arr.max
+  	arr.delete_at(arr.index(max))
+  	return max + arr.max
+  end	
+end
+
+def sum_to_n? (arr, n)
+  if arr.length == 1
+  	return false
   end
-end
 
-def max_2_sum arr
-  # YOUR CODE HERE
-end
+  my_hash = Hash.new(0)
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+  0.upto(arr.length - 1) {
+  	|i|
+  	if (my_hash.value?(n - arr[i]))
+  		return true
+  	end
+  	my_hash[i.to_s.to_sym] = arr[i]
+  }
+  false
 end
-
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " << name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  upper = ['A', 'E', 'I', 'O', 'U']
+  lower = ['a', 'e', 'i', 'o', 'u']
+  if s.length == 0 || !(s[0].to_s =~ /[A-Za-z]/)
+  	return false
+  end
+  return !(upper.include?(s[0].to_s) || lower.include?(s[0].to_s))
 end
 
 def binary_multiple_of_4? s
